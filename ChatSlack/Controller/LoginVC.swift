18adc0c7457f81b6b13e1ecda8f5073b6a2fa60f
Kesley Ribeiro <kesley@appaocubo.com.br>
2017-10-19 20:37:15 -0200

    LoginVC user interface

---
+++ ChatSlack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  ChatSlack
+//
+//  Created by Kesley Ribeiro on 19/Oct/17.
+//  Copyright © 2017 Kesley Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+    
+        dismiss(animated: true, completion: nil)
+    }
+
+}
---
+++ ChatSlack/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ChatSlack
+//
+//  Created by Kesley Ribeiro on 19/Oct/17.
+//  Copyright © 2017 Kesley Ribeiro. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
