7a064ff83c94a5fb1d212f3ac0adc579869f6fc6
Kesley Ribeiro <kesley@appaocubo.com.br>
2017-10-19 13:27:56 -0200

    Initial Commit

---
+++ ChatSlack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatSlack
+//
+//  Created by Kesley Ribeiro on 19/Oct/17.
+//  Copyright © 2017 Kesley Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
